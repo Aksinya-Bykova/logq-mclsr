4f80d2f652ad2af686f0241e336204a0dc5ef60e
Aksinya-Bykova <367121@niuitmo.ru>
2025-12-29 17:24:50 +0300

    more fast metrics from irec with some failures

--- mclsr-exp-LIC/src/irec/metric/base.py
+++ mclsr-exp-LIC/src/irec/metric/base.py
@@ -1,4 +1,4 @@
-from irec.utils import MetaParent
+from irec.utils import MetaParent, create_masked_tensor, DEVICE
 
 import torch
 
@@ -12,7 +12,7 @@ class StatefullMetric(BaseMetric):
         raise NotImplementedError
 
 
-class StaticMetric(BaseMetric, config_name='dummy'):
+class StaticMetric(BaseMetric, config_name="dummy"):
     def __init__(self, name, value):
         self._name = name
         self._value = value
@@ -23,16 +23,14 @@ class StaticMetric(BaseMetric, config_name='dummy'):
         return inputs
 
 
-class CompositeMetric(BaseMetric, config_name='composite'):
+class CompositeMetric(BaseMetric, config_name="composite"):
     def __init__(self, metrics):
         self._metrics = metrics
 
     @classmethod
     def create_from_config(cls, config):
         return cls(
-            metrics=[
-                BaseMetric.create_from_config(cfg) for cfg in config['metrics']
-            ],
+            metrics=[BaseMetric.create_from_config(cfg) for cfg in config["metrics"]],
         )
 
     def __call__(self, inputs):
@@ -41,7 +39,7 @@ class CompositeMetric(BaseMetric, config_name='composite'):
         return inputs
 
 
-class NDCGMetric(BaseMetric, config_name='ndcg'):
+class NDCGMetric(BaseMetric, config_name="ndcg"):
     def __init__(self, k):
         self._k = k
 
@@ -50,9 +48,8 @@ class NDCGMetric(BaseMetric, config_name='ndcg'):
             :,
             : self._k,
         ].float()  # (batch_size, top_k_indices)
-        labels = inputs['{}.ids'.format(labels_prefix)].float()  # (batch_size)
+        labels = inputs["{}.ids".format(labels_prefix)].float()  # (batch_size)
 
-        
         assert labels.shape[0] == predictions.shape[0]
 
         hits = torch.eq(
@@ -61,13 +58,15 @@ class NDCGMetric(BaseMetric, config_name='ndcg'):
         ).float()  # (batch_size, top_k_indices)
         discount_factor = 1 / torch.log2(
             torch.arange(1, self._k + 1, 1).float() + 1.0,
-        ).to(hits.device)  # (k)
-        dcg = torch.einsum('bk,k->b', hits, discount_factor)  # (batch_size)
+        ).to(
+            hits.device
+        )  # (k)
+        dcg = torch.einsum("bk,k->b", hits, discount_factor)  # (batch_size)
 
         return dcg.cpu().tolist()
 
 
-class RecallMetric(BaseMetric, config_name='recall'):
+class RecallMetric(BaseMetric, config_name="recall"):
     def __init__(self, k):
         self._k = k
 
@@ -76,7 +75,7 @@ class RecallMetric(BaseMetric, config_name='recall'):
             :,
             : self._k,
         ].float()  # (batch_size, top_k_indices)
-        labels = inputs['{}.ids'.format(labels_prefix)].float()  # (batch_size)
+        labels = inputs["{}.ids".format(labels_prefix)].float()  # (batch_size)
 
         assert labels.shape[0] == predictions.shape[0]
 
@@ -89,114 +88,122 @@ class RecallMetric(BaseMetric, config_name='recall'):
         return recall.cpu().tolist()
 
 
-class CoverageMetric(StatefullMetric, config_name='coverage'):
+class CoverageMetric(StatefullMetric, config_name="coverage"):
     def __init__(self, k, num_items):
         self._k = k
         self._num_items = num_items
 
     @classmethod
     def create_from_config(cls, config, **kwargs):
-        return cls(k=config['k'], num_items=kwargs['num_items'])
+        return cls(k=config["k"], num_items=kwargs["num_items"])
 
     def __call__(self, inputs, pred_prefix, labels_prefix):
         predictions = inputs[pred_prefix][
             :,
             : self._k,
         ].float()  # (batch_size, top_k_indices)
-        return (
-            predictions.view(-1).long().cpu().detach().tolist()
-        )  # (batch_size * k)
+        return predictions.view(-1).long().cpu().detach().tolist()  # (batch_size * k)
 
     def reduce(self, values):
         return len(set(values)) / self._num_items
 
-class MCLSRNDCGMetric(BaseMetric, config_name='mclsr-ndcg'):
+
+class MCLSRNDCGMetric(BaseMetric, config_name="mclsr-ndcg"):
     def __init__(self, k):
         self._k = k
 
     def __call__(self, inputs, pred_prefix, labels_prefix):
-        predictions = inputs[pred_prefix][:, :self._k] # (batch_size, k)
-        labels_flat = inputs[f'{labels_prefix}.ids']      # (total_labels,)
-        labels_lengths = inputs[f'{labels_prefix}.length'] # (batch_size,)
+        predictions = inputs[pred_prefix][:, : self._k].to(DEVICE)  # (batch_size, k)
+        labels_flat = inputs[f"{labels_prefix}.ids"].to(DEVICE)  # (total_labels,)
+        labels_lengths = inputs[f"{labels_prefix}.length"].to(DEVICE)  # (batch_size,)
 
         assert predictions.shape[0] == labels_lengths.shape[0]
 
-        dcg_scores = []
-        offset = 0
-        for i in range(predictions.shape[0]):
-            user_predictions = predictions[i]
-            num_user_labels = labels_lengths[i]
-            user_labels = labels_flat[offset : offset + num_user_labels]
-            offset += num_user_labels
+        batch_size = predictions.shape[0]
 
-            hits_mask = torch.isin(user_predictions, user_labels) # (k,) -> True/False
-            
-            positions = torch.arange(2, self._k + 2, device=predictions.device)
-            weights = 1 / torch.log2(positions.float())
-            dcg = (hits_mask.float() * weights).sum()
-            
-            num_ideal_hits = min(self._k, num_user_labels)
-            idcg_weights = weights[:num_ideal_hits]
-            idcg = idcg_weights.sum()
-            
-            ndcg = dcg / idcg if idcg > 0 else torch.tensor(0.0)
-            dcg_scores.append(ndcg.cpu().item())
-            
-        return dcg_scores
+        padded_labels, labels_mask = create_masked_tensor(
+            data=labels_flat, lengths=labels_lengths
+        )
+        padded_labels[~labels_mask] = -1
+
+        positions = torch.arange(2, self._k + 2, device=predictions.device)
+        weights = 1.0 / torch.log2(positions.float())
+
+        is_hit = (predictions[:, :, None] == padded_labels[:, None, :]).sum(
+            dim=-1
+        )  # (batch_size, k)
+
+        num_ideal_hits = torch.minimum(
+            labels_lengths,
+            torch.as_tensor(
+                self._k, device=labels_lengths.device, dtype=labels_lengths.dtype
+            ),
+        )  # (batch_size)
+        ideal_mask = (
+            torch.arange(self._k, device=is_hit.device, dtype=weights.dtype)[
+                None, :
+            ].tile(dims=[batch_size, 1])
+            < num_ideal_hits[:, None]
+        )  # (batch_size, k)
+
+        dcg = (is_hit.float() * weights).sum(dim=-1)  # (batch_size)
+        idcg = (ideal_mask.float() * weights).sum(dim=-1)  # (batch_size)
+
+        ndcg = dcg / idcg
+
+        return ndcg.tolist()
 
 
-class MCLSRRecallMetric(BaseMetric, config_name='mclsr-recall'):
+class MCLSRRecallMetric(BaseMetric, config_name="mclsr-recall"):
     def __init__(self, k):
         self._k = k
 
     def __call__(self, inputs, pred_prefix, labels_prefix):
-        predictions = inputs[pred_prefix][:, :self._k] # (batch_size, k)
-        labels_flat = inputs[f'{labels_prefix}.ids']      # (total_labels,)
-        labels_lengths = inputs[f'{labels_prefix}.length'] # (batch_size,)
+        predictions = inputs[pred_prefix][:, : self._k].to(DEVICE)  # (batch_size, k)
+        labels_flat = inputs[f"{labels_prefix}.ids"].to(DEVICE)  # (total_labels,)
+        labels_lengths = inputs[f"{labels_prefix}.length"].to(DEVICE)  # (batch_size,)
 
         assert predictions.shape[0] == labels_lengths.shape[0]
 
-        recall_scores = []
-        offset = 0
-        for i in range(predictions.shape[0]):
-            user_predictions = predictions[i]
-            num_user_labels = labels_lengths[i]
-            user_labels = labels_flat[offset : offset + num_user_labels]
-            offset += num_user_labels
-            
-            hits = torch.isin(user_predictions, user_labels).sum().float()
-            
-            recall = hits / num_user_labels if num_user_labels > 0 else torch.tensor(0.0)
-            recall_scores.append(recall.cpu().item())
-        
-        return recall_scores
+        padded_labels, labels_mask = create_masked_tensor(
+            data=labels_flat, lengths=labels_lengths
+        )
+        padded_labels[~labels_mask] = -1
 
-class MCLSRHitRateMetric(BaseMetric, config_name='mclsr-hit'):
+        is_hit = (
+            (predictions[:, :, None] == padded_labels[:, None, :]).sum(dim=-1).float()
+        )  # (batch_size, k)
+        recall = is_hit.sum(dim=-1) / torch.minimum(
+            labels_lengths,
+            torch.as_tensor(
+                self._k, device=labels_lengths.device, dtype=labels_lengths.dtype
+            ),
+        )  # (batch_size)
+
+        return recall.tolist()
+
+
+class MCLSRHitRateMetric(BaseMetric, config_name="mclsr-hit"):
     def __init__(self, k):
         self._k = k
 
     def __call__(self, inputs, pred_prefix, labels_prefix):
-        predictions = inputs[pred_prefix][:, :self._k] # (batch_size, k)
-        labels_flat = inputs[f'{labels_prefix}.ids']      # (total_labels,)
-        labels_lengths = inputs[f'{labels_prefix}.length'] # (batch_size,)
+        predictions = inputs[pred_prefix][:, : self._k].to(DEVICE)  # (batch_size, k)
+        labels_flat = inputs[f"{labels_prefix}.ids"].to(DEVICE)  # (total_labels,)
+        labels_lengths = inputs[f"{labels_prefix}.length"].to(DEVICE)  # (batch_size,)
 
         assert predictions.shape[0] == labels_lengths.shape[0]
 
-        hit_scores = []
-        offset = 0
-        for i in range(predictions.shape[0]):
-            user_predictions = predictions[i]
-            num_user_labels = labels_lengths[i]
+        padded_labels, labels_mask = create_masked_tensor(
+            data=labels_flat, lengths=labels_lengths
+        )
+        padded_labels[~labels_mask] = -1
 
-            if num_user_labels == 0:
-                hit_scores.append(0.0)
-                continue
+        hit_rate = (
+            (predictions[:, :, None] == padded_labels[:, None, :])
+            .sum(dim=-1)
+            .max(dim=-1)
+            .values.float()
+        )  # (batch_size)
 
-            user_labels = labels_flat[offset : offset + num_user_labels]
-            offset += num_user_labels
-            
-            is_hit = torch.isin(user_predictions, user_labels).any()
-            
-            hit_scores.append(float(is_hit))
-        
-        return hit_scores
+        return hit_rate.tolist()
