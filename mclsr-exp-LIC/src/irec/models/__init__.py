e081fb42a588f268fc0e92be5a1edde825239b63
Aksinya-Bykova <367121@niuitmo.ru>
2025-12-29 17:42:05 +0300

    delete include sasrec

--- mclsr-exp-LIC/src/irec/models/__init__.py
+++ mclsr-exp-LIC/src/irec/models/__init__.py
@@ -1,7 +1,5 @@
 from .base import BaseModel, SequentialTorchModel
 from .mclsr import MCLSRModel
-from .sasrec import SasRecModel, SasRecInBatchModel
-from .sasrec_ce import SasRecCeModel
 
 __all__ = [
     'BaseModel',
