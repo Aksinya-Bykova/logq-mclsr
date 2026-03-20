0e6b123219c2389835d3d4f65725a9947079f4d8
Aksinya-Bykova <367121@niuitmo.ru>
2026-03-20 21:03:27 +0300

    delete useless directory

--- optimize/src/irec/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import BaseModel, SequentialTorchModel
-from .mclsr import MCLSRModel
-from .sasrec import SasRecModel, SasRecInBatchModel
-
-__all__ = [
-    'BaseModel',
-    'MCLSRModel',
-    'SasRecModel',
-]
